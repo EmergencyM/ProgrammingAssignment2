c06bc74b54d6f9e47b16d954382764290bf1a233
EmergencyM <mhinson81@gmail.com>
2014-12-20 09:22:38 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below allow you to potentially cut computation time on mean calculations if the data hasn't change.  
+## It will use a cached mean instead of calculating, this is particularly useful in looping.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse of the matrix
+## 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -16,7 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
   setinverse = setinverse,
   getinverse = getinverse)}
 
-## Write a short comment describing this function
+##The following function calculates the mean of the special "vector" created with makeCacheMatrix. 
+##It checks to see if the mean has already been calculated.
+##If so, it gets the mean from the cache and skips the computation. 
+##Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
